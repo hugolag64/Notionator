11cf87d24dfb8326dd888632c2e1091935dc685a
hugolag64 <hugolagpaypal@gmail.com>
2025-07-26 17:33:28 +0400

    Mise à jour complète du projet

--- ui/semestre_view.py
+++
@@ -1,6 +0,0 @@
-import customtkinter as ctk
-
-class SemestreView(ctk.CTkFrame):
-    def __init__(self, master):
-        super().__init__(master)
-        ctk.CTkLabel(self, text="Semestre", font=("Helvetica", 18, "bold")).pack(pady=10)
